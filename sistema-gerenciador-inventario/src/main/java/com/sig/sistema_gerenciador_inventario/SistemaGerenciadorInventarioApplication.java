2b957d0a0632b482bc56a42d3e59b8408c28ae21
Vinicius Pedro Andreazza <viniciuslandreazza@gmail.com>
2026-01-27 20:11:03 -0300

    Initial structure

---
+++ sistema-gerenciador-inventario/src/main/java/com/sig/sistema_gerenciador_inventario/SistemaGerenciadorInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.sig.sistema_gerenciador_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciadorInventarioApplication.class, args);
+	}
+
+}
---
+++ sistema-gerenciador-inventario/src/test/java/com/sig/sistema_gerenciador_inventario/SistemaGerenciadorInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sig.sistema_gerenciador_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
