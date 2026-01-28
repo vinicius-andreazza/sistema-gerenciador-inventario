b05c5e671baadc3ee427d0a8d0dcb27e5637cb98
Vinicius Pedro Andreazza <viniciuslandreazza@gmail.com>
2026-01-28 10:48:15 -0300

    add: CustomUserDetails and SecurityConfig

--- sistema-gerenciador-inventario/src/main/java/com/sig/sistema_gerenciador_inventario/security/CustomUserDetailsService.java
+++ sistema-gerenciador-inventario/src/main/java/com/sig/sistema_gerenciador_inventario/security/CustomUserDetailsService.java
@@ -5,13 +5,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        User user = userRepository.findByName(username);
+        return UserPrincipal.builder().username(user.getName()).password(user.getPassword()).authorities(user.getRoles()).build();
     }
     
 }
--- sistema-gerenciador-inventario/src/main/java/com/sig/sistema_gerenciador_inventario/security/SecurityConfig.java
+++ sistema-gerenciador-inventario/src/main/java/com/sig/sistema_gerenciador_inventario/security/SecurityConfig.java
@@ -2,19 +2,56 @@ package com.sig.sistema_gerenciador_inventario.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sig.sistema_gerenciador_inventario.security.jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .authenticationManager(null)
+                .authorizeHttpRequests(
+                    auth -> auth
+                    .requestMatchers("/**").permitAll()
+                    .anyRequest().authenticated()
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
